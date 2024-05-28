fea60f7b545e6604a67d2bb60a2eacf8d2cec6fe
Raghad-ali-Alotaibi <raghad.ali.alotaibi@gmail.com>
2024-05-28 20:58:01 +0300

    add link4

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 const isDevelopment = import.meta.env.MODE === "development"
-let baseURL = "https://sda-online-2-csharp-backend-teamworkapi.onrender.com/"
+let baseURL = "https://sda-online-2-csharp-backend-teamworkapi.onrender.com/api"
 
 if (!isDevelopment) {
   // Update this later when you have a working backend server
-  baseURL = "https://sda-online-2-csharp-backend-teamworkapi.onrender.com/"
+  baseURL = "https://sda-online-2-csharp-backend-teamworkapi.onrender.com/api"
 }
 
 const api = axios.create({
