dbd368b589906a4e473fada30e0528c0be39a164
Raghad-ali-Alotaibi <raghad.ali.alotaibi@gmail.com>
2024-05-28 20:34:43 +0300

    add link3

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
-const isDevelopment = import.meta.env.MODE === 'development'
-let baseURL = 'https://sda-online-2-csharp-backend-teamworkapi.onrender.com/'
+const isDevelopment = import.meta.env.MODE === "development"
+let baseURL = "https://sda-online-2-csharp-backend-teamworkapi.onrender.com/"
 
 if (!isDevelopment) {
   // Update this later when you have a working backend server
-  baseURL = 'https://sda-online-2-csharp-backend-teamworkapi.onrender.com/'
+  baseURL = "https://sda-online-2-csharp-backend-teamworkapi.onrender.com/"
 }
 
 const api = axios.create({
