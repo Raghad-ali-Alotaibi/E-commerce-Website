ac9e18e15fd6cf405811d272e9753dac72ce3d1e
Raghad-ali-Alotaibi <raghad.ali.alotaibi@gmail.com>
2024-05-28 17:36:24 +0300

    fix some errors3

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,16 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      // Add alias for "@fortawesome/fontawesome-svg-core"
-      "@fortawesome/fontawesome-svg-core": "@fortawesome/fontawesome-svg-core/index.es.js"
-    }
+    },
   },
-  build: {
-    rollupOptions: {
-      external: ["@fortawesome/fontawesome-svg-core"]
-    }
-  }
+  optimizeDeps: {
+    // Provide a map to resolve the bare specifier
+    include: ["@fortawesome/fontawesome-svg-core"],
+  },
 });
